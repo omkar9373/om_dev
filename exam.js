86ce0064a243cf5e606dad90357ba2e8435a1782
omkar <omkarnjadhav2612@gmail.com>
2025-08-07 01:51:33 +0530

    practice js

--- exam.js
+++ exam.js
@@ -1,5 +1,29 @@
-document.getElementById("btn").addEventListener("click",function(){
-     var name = document.getElementById("username").value
-     document.write("welcome"+ "  " +name)
-     localStorage.setItem('name',name);
-     sessionStorage.setItem('name',name);})
+function savevalue() {
+    // document.getElementById("btn").addEventListener("click", function (e) {
+    //     e.preventDefault();
+        var user = document.getElementById("username").value;
+
+
+        var pass = document.getElementById("password").value;
+        // var sub = document.getElementById("sub").value;
+
+        // let arry = [user, pass, sub];
+        console.log("Name:-",user);
+    // })
+
+}
+
+function printPara(){
+
+    var printPara = document.getElementById("paraIdFont").outerHTML
+    console.log(printPara)
+
+}
+function enterMouse(){
+    var entry = document.getElementById("boxId").innerText
+    console.log("Enter mouse",entry)
+}
+function leaveMouse(){
+    var outsideEntry = document.getElementById("boxId").innerText
+    console.log("mouse Out",outsideEntry)
+}
