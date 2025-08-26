0fe576eff2ab9b42527cbe055cb1a080907a1bc9
omkar <omkarnjadhav2612@gmail.com>
2025-08-26 20:59:42 +0530

    change

--- cal.js
+++ cal.js
@@ -26,9 +26,135 @@
 //     })
     
 
-let a=[11,25,42,15,36,75];
-   let d= a.filter((val)=>{
-let c= val%2===0;
-    })
-    console.log(d);
+// let a=[11,25,42,15,36,75];
+//    let d= a.filter((val)=>{
+// let c= val %2===0;
+//     })
+//     console.log(d);
     
+//     let d=a.filter((val)=>{      //filter
+//         if (val)
+//         return()
+
+//     })
+// const employees = [
+//   { id: 1, name: "Amit", department: "IT", salary: 60000, yearsOfExperience: 4 },
+//   { id: 2, name: "Priya", department: "HR", salary: 45000, yearsOfExperience: 2 },
+//   { id: 3, name: "Rahul", department: "Finance", salary: 75000, yearsOfExperience: 5 },
+//   { id: 4, name: "Sneha", department: "IT", salary: 90000, yearsOfExperience: 7 },
+//   { id: 5, name: "Karan", department: "Marketing", salary: 50000, yearsOfExperience:3},
+// ];
+// console.log(employees);
+
+// const employees = [
+//   { id: 1, name: "Amit", department: "IT", salary: 60000, yearsOfExperience: 4 },
+//   { id: 2, name: "Priya", department: "HR", salary: 45000, yearsOfExperience: 2 },
+//   { id: 3, name: "Rahul", department: "Finance", salary: 75000, yearsOfExperience: 5 },
+//   { id: 4, name: "Sneha", department: "IT", salary: 90000, yearsOfExperience: 7 },
+//   { id: 5, name: "Karan", department: "Marketing", salary: 50000, yearsOfExperience: 3 },
+// ];
+
+// const result = employees 
+// .filter(emp => emp.department === "IT" & emp.yearsOfExperience > 3)
+//   .map(emp => ({
+//     name: emp.name.toUpperCase(),
+//     bonus: emp.salary * 0.10
+//   }));                               ///map
+
+// console.log(result);
+
+
+
+
+
+// const products = [
+//   { id: 1, name: "Laptop", category: "Electronics", price: 55000, stock: 5 },
+//   { id: 2, name: "Phone", category: "Electronics", price: 25000, stock: 0 },
+//   { id: 3, name: "Shoes", category: "Fashion", price: 3000, stock: 20 },
+//   { id: 4, name: "Watch", category: "Fashion", price: 7000, stock: 15 },
+//   { id: 5, name: "Headphones", category: "Electronics", price: 4000, stock: 8 },
+// ];
+
+// const result = products
+  
+//   .filter(prod => prod.category === "Electronics" && prod.stock > 0)
+
+
+// let a=[1,2,3,4];
+// let arr= a.map( x => x+2);
+//  console.log(arr);
+
+// let ba=[2,5,6,7];
+//  let =[a,b,c,d]=ba;
+//   console.log(ba);      //array desructuring
+
+
+// let obj ={ a:5,b:6,k:8};
+//  let {a,b,k} = obj;
+//  console.log(b);       // obj destrucring
+
+//  let a = [1,2,3];
+//  let b = ["length","history","f"];
+//   let result=[...a,b];             // rest marg to arry
+//   console.log(result);
+       
+
+//practice to day//
+
+
+
+
+// let team_cricket=['virat','rohit','dhoni','dhavan','hardik','kunal'] ;
+
+//  let number=[18,45,7,11,19,29];                                             //
+//   console.log(45);
+  
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// let array = arr.filter 
+
+
+// var x=1;
+// function foo (){
+//     console.log(x);
+//     var x=2 ;   
+//     console.log(x);
+// }
+// foo()
+
+
+// for (var i=0;i<3;i++){
+//     setTimeout(()=>
+//     console.log(i)1000);
+// }\
+
+// let a=[1,2,3];
+// let b=a;
+//  b.push(4)
+//  console.log(a,b);
+ 
+
+// let student={
+//     name:'om',
+//     age:21,
+//     marks:85,
+// };
+// console.log(student.name);
+
+
+
+// let arr = [1,2,3,4,5];
+// arr.sort((a, b) => b - a);
+
+// console.log(arr);
+
+
+// let a=[1,2];
+// let b=[3,4];
+
+// let c=[...a,...b];
+// console.log(c);
+
+
